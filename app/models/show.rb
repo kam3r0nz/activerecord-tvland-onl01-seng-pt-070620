44d918ecb5e65820131bd6a10d0cd3b21d380920
kam3r0nz <kam.hungerford@gmail.com>
2020-09-15 15:33:15 -0500

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,12 @@
 class Show < ActiveRecord::Base
-  
+    attr_accessor :season
+
+    has_many :characters
+    belongs_to :network
+
+    def actors_list
+        self.characters.collect do |character|
+            character.actor.full_name
+        end
+    end
 end
